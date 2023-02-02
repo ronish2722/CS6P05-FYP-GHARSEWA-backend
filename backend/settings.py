f01a99952ddbbd081396d3d62db33dc92ed6557e
ronish2722 <ronishshrestha2722@gmail.com>
2023-02-02 12:44:50 +0545

    backend for login

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
 
     'rest_framework',
+
+    'rest_framework_simplejwt.token_blacklist',
+
+    "corsheaders",
 ]
 
 REST_FRAMEWORK = {
@@ -88,12 +92,18 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -168,3 +178,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
