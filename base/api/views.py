a87677a939ac1d3a765b5fd5175b1ddf869cffe4
ronish2722 <ronishshrestha2722@gmail.com>
2023-02-07 12:20:31 +0545

    dennis ivy login finished

--- base/api/views.py
+++ base/api/views.py
@@ -1,10 +1,14 @@
+from base.models import Note
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from.serializers import NoteSerializer
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -32,3 +36,12 @@ def getRoutes(request):
 
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    user = request.user
+    notes = user.note_set.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
