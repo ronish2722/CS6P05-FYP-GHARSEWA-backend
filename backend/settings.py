19d222838aaba134f310bf8269ae7177ffc2d5ef
ronish2722 <ronishshrestha2722@gmail.com>
2023-02-09 10:06:13 +0545

    connected to database

--- backend/settings.py
+++ backend/settings.py
@@ -132,8 +132,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gharsewa',
+        'USER': 'root',
+        'PASSWORD': 'ronish',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
     }
 }
 
