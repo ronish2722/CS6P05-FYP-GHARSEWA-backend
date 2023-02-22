6d60fae2a30f1b8a7e3637e1fc7b316e75177afe
ronish2722 <ronishshrestha2722@gmail.com>
2023-02-22 15:51:01 +0545

    Created Review and Book table

--- base/admin.py
+++ base/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Task, Professional
-admin.site.register(Task, Professional)
+# from .models import Task
+# admin.site.register(Task)
+
+
+from .models import *
+admin.site.register(Professional)
+admin.site.register(Review)
+admin.site.register(Book)
--- base/models.py
+++ base/models.py
@@ -17,6 +17,39 @@ class Professional(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    professional = models.ForeignKey(
+        Professional, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.rating)
+
+
+class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    paymentMethod = models.CharField(max_length=200, null=True, blank=True)
+    totalPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    isPaid = models.BooleanField(default=False)
+    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    isCompleted = models.BooleanField(default=False)
+    completedAt = models.DateTimeField(
+        auto_now_add=False, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.createdAt)
+
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
